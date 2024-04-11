3fe9861b04cb6ab6372312aa08a3a882141b188d
Chamroeun phalpanha <162548211+Phalpanha9999@users.noreply.github.com>
2024-04-11 15:13:22 +0700

    Add files via upload

---
+++ Homerwork 1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number1;
+	int number2;
+	int number3;
+	int total;
+
+	    cout<<"-----Calculator-----"<<endl;
+     	cout<<"please enter 2 number to calculator:"<<endl;
+	    cout<<"First Number =";
+    	cin>>number1;
+    	cout<<"Second Number =";
+    	cin>>number2;
+    	cout<<"third Number=";
+    	
+ 	total=number1+number2+number3;
+	
+	cout<<"Total ="<<total;
+	
+	return 0;
+}
